339f5bdc6dc0be6b305d945c6ddff8d33592710f
luca-moraes <lcansd.lm@gmail.com>
2023-10-23 04:46:52 -0300

    paralax incial

---
+++ SpaceShip/Assets/Scripts/Paralax.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paralax : MonoBehaviour
+{
+  	private float lenght, startpos;
+    private float movingSpeed = 5f;
+    public GameObject cam;
+    public float parallaxEffect;
+
+  	void Start()
+   	{
+       startpos = transform.position.x;
+       lenght = GetComponent<SpriteRenderer>().bounds.size.x;
+	}
+
+	void Update()
+    {
+		transform.position += Vector3.left * Time.deltaTime * movingSpeed * parallaxEffect;
+		if(transform.position.x < startpos - 2*lenght ) transform.position = new Vector3(startpos, transform.position.y, transform.position.z);
+    }
+}
