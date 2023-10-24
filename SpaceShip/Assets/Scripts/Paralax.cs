6453896e8f5f4ce7570f7f539468bc784cdf3d2c
luca-moraes <lcansd.lm@gmail.com>
2023-10-23 21:51:46 -0300

    correcao de script e cenas de paralaxe

--- SpaceShip/Assets/Scripts/Paralax.cs
+++ SpaceShip/Assets/Scripts/Paralax.cs
@@ -4,20 +4,22 @@ using UnityEngine;
 
 public class Paralax : MonoBehaviour
 {
-  	private float lenght, startpos;
-    private float movingSpeed = 5f;
-    public GameObject cam;
-    public float parallaxEffect;
+  private float lenght, startpos;
+  private float movingSpeed = 5f;
+  public GameObject cam;
+  public float parallaxEffect;
 
-  	void Start()
-   	{
-       startpos = transform.position.x;
-       lenght = GetComponent<SpriteRenderer>().bounds.size.x;
+  void Start()
+  {
+    startpos = transform.position.x;
+    lenght = GetComponent<SpriteRenderer>().bounds.size.x;
 	}
 
 	void Update()
-    {
-		transform.position += Vector3.left * Time.deltaTime * movingSpeed * parallaxEffect;
-		if(transform.position.x < startpos - 2*lenght ) transform.position = new Vector3(startpos, transform.position.y, transform.position.z);
+  {
+    transform.position += Vector3.left * Time.deltaTime * movingSpeed * parallaxEffect;
+    if(transform.position.x < startpos - lenght ){
+      transform.position = new Vector3(startpos, transform.position.y, transform.position.z);
     }
+  }
 }
